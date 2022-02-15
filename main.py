2fcf659295696db45d22c4b85c933932188b460d
karthickraja13 <98940562+karthickraja13@users.noreply.github.com>
2022-02-15 13:42:10 +0530

    Update main.py
    1.edit the all program.
    2.this is a formatting string programing file.

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-# This is a sample Python script.
+EXAMPLE:1
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+price=678
+text=("i went the market the one purches a remote={}")
+print(text.format(price))
+text=("i went the market the one purches a remote={:.1f}")
+print(text.format(price))
 
+EXAMPLE:2
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+quantity=3
+itemno=10
+price=89
+text=("i went the {} pieces of the item naumber {} for={:.2f}")
+print(text.format(quantity,itemno,price))
 
+EXAMPLE:3
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+age=23
+name="karthick"
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+text=("he,is {1}.his is age{0}")
+print(text.format(age,name))
+
+EXAMPLE:4
+text=("he is a new bike purches {name} the model {model}")
+print(text.format(name="yammaga",model="alpha"))
